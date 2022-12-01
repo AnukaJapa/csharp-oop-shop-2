239313feb050882abcc9f1e369bd11f17b64000f
AnukaJapa <japaridze.ana96@gmail.com>
2022-12-01 15:48:07 +0100

    completato metodo Riempi

--- CSharpShop2/Acqua.cs
+++ CSharpShop2/Acqua.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,22 +9,63 @@ namespace CSharpShop2
 {
     public class Acqua : Prodotto
     {
-        protected int litri;
-        protected int massimoContenutoLitri; 
-        public Acqua(int prezzo, int iva, int litri, string nome = "prodotto") : base(prezzo, iva, nome)
+        protected double litri;
+        protected int ph; 
+        public Acqua(int prezzo, int iva, double litri, int ph, string nome = "prodotto") : base(prezzo, iva, nome)
         {
-            this.litri = litri;
+            
+           SetLitri(litri);
+            this.ph= ph;
+
         }
 
         // GETTERS
-        public int GetLitri()
+        public double GetLitri()
         {
             return this.litri;
         }
-        //SETTERS
-        public int SetLitri(int litri)
+
+        public int GetPhAcqua()
         {
-            this
+            return this.ph;
+        }
+        //SETTERS
+        public void SetLitri(double litri)
+        {
+
+            if (litri > 1.5 || litri <= 0)
+            {
+                Console.WriteLine("Il prodotto Acqua non può avere come contenuto meno di 0 litri di acqua o più di 1.5 litri");
+
+       
+            }
+            else
+            {
+                this.litri = litri;
+            }
+
+        }
+
+        public void Bevi(double litriDaBere)
+        {
+            if(this.litri -litriDaBere > 0)
+            {
+                this.litri = litri - litriDaBere;
+            } else
+            {
+                this.litri = 0;
+            }
+        }
+
+        public void Riempi(double litriDaRiempire)
+        {
+            if(this.litri + litriDaRiempire > 1.5)
+            {
+                this.litri = 1.5;
+            }else
+            {
+                this.litri = this.litri + litriDaRiempire; 
+            }
         }
 
     }
