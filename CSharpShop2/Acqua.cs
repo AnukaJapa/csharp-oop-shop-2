41f547a0366386fef2cd0f42e1ed31307fe7e73f
AnukaJapa <japaridze.ana96@gmail.com>
2022-12-01 14:58:08 +0100

    prime modifiche

---
+++ CSharpShop2/Acqua.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop2
+{
+    public class Acqua : Prodotto
+    {
+        protected int litri;
+        protected int massimoContenutoLitri; 
+        public Acqua(int prezzo, int iva, int litri, string nome = "prodotto") : base(prezzo, iva, nome)
+        {
+            this.litri = litri;
+        }
+
+        // GETTERS
+        public int GetLitri()
+        {
+            return this.litri;
+        }
+        //SETTERS
+        public int SetLitri(int litri)
+        {
+            this
+        }
+
+    }
+}
